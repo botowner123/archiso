9d401c04a1e81fcebc2a0725b987c82063334f1b
botowner123 <minhkhoit269@gmail.com>
2026-04-02 07:26:59 -0700

    Update launcher.sh

--- configs/releng/airootfs/etc/skel/.config/polybar/launcher.sh
+++ configs/releng/airootfs/etc/skel/.config/polybar/launcher.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# 1. Terminate already running bar instances
+# Terminate already running bar instances
 killall -q polybar
 
-# 2. Wait until the processes have been shut down
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# 3. Launch the bar (using the name of your bar from config.ini)
+# Launch Polybar
+# IMPORTANT: Check your config.ini! If your bar is named [bar/main], 
+# change 'example' to 'main' below.
 polybar mainbar &
